7908656b23d4500b0d48e90db2d9fe9d676eee86
BROWN <63453889+brownliuinnz@users.noreply.github.com>
2020-07-29 11:31:25 +1200

    leetcode

--- 20_Valid_Parentheses.py
+++ 20_Valid_Parentheses.py
@@ -12,3 +12,16 @@ class Solution:
                 if left.index(stack.pop()) != right.index(char):
                     return False
         return len(stack) == 0
+
+    def isValidAlternative(self, s: str) -> bool:
+        bracket_map = {"(": ")", "[": "]", "{": "}"}
+        open_par = set(["(", "[", "{"])
+        stack = []
+        for i in s:
+            if i in open_par:
+                stack.append(i)
+            elif stack and i == bracket_map[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        return stack == []
