4ce20fc8ee5d13fd5be5c99083e2761c79873a82
Brown <brownliuinnz@gmail.com>
2020-07-31 23:31:36 +1200

    updates

--- sandbox.py
+++ sandbox.py
@@ -8,10 +8,16 @@
 # A=[1,2,3,4]
 #
 # print(A[-1])
+#
+# a= '3'
+# b=''
+#
+# if not a or not b:
+#     print(a or b)
 
-a= '3'
-b=''
-
-if not a or not b:
-    print(a or b)
-
+a=[4,2,3,1,1,14]
+a.remove(1)
+b=a.count(4)
+a.sort()
+print(a)
+print(b)
